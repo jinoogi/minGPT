dabf7577d6147a5d01284e9e026d3445d5c98e92
jinwook <mech1011@naver.com>
2026-02-17 22:08:25 +0900

    CausalSelfAttention클래스 분석

--- mingpt/model.py
+++ mingpt/model.py
@@ -35,6 +35,7 @@ class CausalSelfAttention(nn.Module):
 
     def __init__(self, config):
         super().__init__()
+        # h_size를 C//n_head로 만들려고 
         assert config.n_embd % config.n_head == 0
         # key, query, value projections for all heads, but in a batch
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
@@ -43,28 +44,30 @@ class CausalSelfAttention(nn.Module):
         # regularization
         self.attn_dropout = nn.Dropout(config.attn_pdrop)
         self.resid_dropout = nn.Dropout(config.resid_pdrop)
-        # causal mask to ensure that attention is only applied to the left in the input sequence
-        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
-                                     .view(1, 1, config.block_size, config.block_size))
+        # causalmask를 생성하고 model이 관리하는 buffer에 등록해줌. 
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
         self.n_head = config.n_head
         self.n_embd = config.n_embd
 
     def forward(self, x):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
 
-        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k ,v  = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        # linear층은 입력의 마지막 차원에 대해서 연산을 수행한다고 함.
+        q, k ,v  = self.c_attn(x).split(self.n_embd, dim=2) # (B, T, C)  원래는 (B, T, 3C)인데 -> C 크기대로 분할했으니까... 
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, n_head, T, h_size)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
 
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-        att = F.softmax(att, dim=-1)
+        # causal masking 수행
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf')) # (B, nh, T, T)
+        att = F.softmax(att, dim=-1) # (B, nh, T, T)
         att = self.attn_dropout(att)
+        # 행공간 관점에서의 행렬곱
         y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        # 멀티헤드로 쪼개져있던 z들 다시 합침
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # (B, T, C)
 
         # output projection
         y = self.resid_dropout(self.c_proj(y))
